dcf83a532d79b78f5cd30dbd0be0b908eee6e762
KaonashiMFG <dharmasenaakmalaji@outlook.com>
2024-10-29 16:11:14 +0700

    ada lagi

--- 1-programming-fundamental/intro.js
+++ 1-programming-fundamental/intro.js
@@ -3,3 +3,13 @@
 console.log("lorem ipsum");
 console.log("hello world");
 console.log(21-9+30)
+
+const alamat = "dharmas";
+console.log(alamat)
+
+var dumbo=31
+let akmal=91
+
+{
+    console.log(akmal-dumbo)
+}
